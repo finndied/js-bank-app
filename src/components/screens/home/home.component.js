8eead6983801b278f6af9064e25437d93d2c13a1
Ilya Morozov <finnlivefx1@mail.ru>
2023-05-17 20:18:40 +0700

    Add html to element and random component name

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,11 +1,16 @@
-import { BaseScreen } from "@/core/component/base-screen.component";
+import { BaseScreen } from '@/core/component/base-screen.component'
+import renderService from '@/core/services/render.service'
+
+import template from './home.template.html'
+import styles from './home.module.scss'
 
 export class Home extends BaseScreen {
-   constructor() {
-      super({title: 'Home'})
-   }
+	constructor() {
+		super({ title: 'Home' })
+	}
 
-   render(){
-      return '<p>Home</p>'
-   }
+	render() {
+		const element = renderService.htmlToElement(template, [], styles)
+		return element.outerHTML
+	}
 }
