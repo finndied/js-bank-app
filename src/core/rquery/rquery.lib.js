b400ebd2322555dde5b8d2f2ea307eccb34b8d02
Ilya Morozov <finnlivefx1@mail.ru>
2023-05-18 19:42:38 +0700

    Created jQuery-inspired library

---
+++ src/core/rquery/rquery.lib.js
@@ -0,0 +1,38 @@
+class RQuery {
+	constructor(selector) {
+		if (typeof selector === 'string') {
+			this.element = document.querySelector(selector)
+
+			if (!this.element) {
+				throw new Error(`Element ${selector} not found!`)
+			}
+		} else if (selector instanceof HTMLElement) {
+			this.element = selector
+		} else {
+			throw new Error('Invalid selector type')
+		}
+	}
+
+	find(selector) {
+		const element = new RQuery(this.element.querySelector(selector))
+
+		if (element) {
+			return element
+		} else {
+			throw new Error(`Element ${selector} not found!`)
+		}
+	}
+
+	css(property, value) {
+		if (typeof property !== 'string' || typeof value !== 'string') {
+			throw new Error('property and value must be strings')
+		}
+
+		this.element.style[property] = value
+		return this
+	}
+}
+
+export function $R(selector) {
+	return new RQuery(selector)
+}
