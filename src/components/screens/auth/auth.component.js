6b6c8d9d419c9f9bc391089cef18bac58d2a6b3b
Ilya Morozov <finnlivefx1@mail.ru>
2023-05-18 22:04:19 +0700

    Create components in header

--- src/components/screens/auth/auth.component.js
+++ src/components/screens/auth/auth.component.js
@@ -1,11 +1,23 @@
-import { BaseScreen } from "@/core/component/base-screen.component";
+import { BaseScreen } from '@/core/component/base-screen.component'
+import renderService from '@/core/services/render.service'
+
+import { Heading } from '@/components/ui/heading/heading.component'
+
+import styles from './auth.module.scss'
+import template from './auth.template.html'
 
 export class Auth extends BaseScreen {
-   constructor() {
-      super({ title: 'Auth' })
-   }
-   
-   render(){
-      return '<p>Auth</p>'
-   }
+	constructor() {
+		super({ title: 'Auth' })
+	}
+
+	render() {
+		this.element = renderService.htmlToElement(
+			template,
+			[new Heading('Auth')],
+			styles
+		)
+
+		return this.element
+	}
 }
--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,8 +1,5 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
-import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
-
-import { UserItem } from '@/components/ui/user-item/user-item.component'
 
 import styles from './home.module.scss'
 import template from './home.template.html'
@@ -13,19 +10,7 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(
-			template,
-			[
-				new UserItem({
-					avatarPath:
-						'https://news.workforce.com/wp-content/uploads/sites/2/2018/12/AI-is-coming-%E2%80%94-and-HR-is-not-prepared.jpg',
-					name: 'Finn'
-				})
-			],
-			styles
-		)
-
-		$R(element).find('h1').css('color', 'white')
+		const element = renderService.htmlToElement(template, [], styles)
 
 		return element
 	}
