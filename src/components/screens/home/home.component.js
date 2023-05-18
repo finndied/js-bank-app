fc5b2729e552a604f38fea88422abd4087c36d59
Ilya Morozov <finnlivefx1@mail.ru>
2023-05-18 21:19:27 +0700

    Create ui components

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,8 +1,11 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
+import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
 
-import template from './home.template.html'
+import { UserItem } from '@/components/ui/user-item/user-item.component'
+
 import styles from './home.module.scss'
+import template from './home.template.html'
 
 export class Home extends BaseScreen {
 	constructor() {
@@ -10,7 +13,20 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(template, [], styles)
+		const element = renderService.htmlToElement(
+			template,
+			[
+				new UserItem({
+					avatarPath:
+						'https://news.workforce.com/wp-content/uploads/sites/2/2018/12/AI-is-coming-%E2%80%94-and-HR-is-not-prepared.jpg',
+					name: 'Finn'
+				})
+			],
+			styles
+		)
+
+		$R(element).find('h1').css('color', 'white')
+
 		return element
 	}
 }
--- src/core/rquery/rquery.lib.js
+++ src/core/rquery/rquery.lib.js
@@ -1,3 +1,5 @@
+import { formatCardNumberWithDashes } from '@/utils/format/format.card.number'
+
 class RQuery {
 	constructor(selector) {
 		if (typeof selector === 'string') {
@@ -13,6 +15,8 @@ class RQuery {
 		}
 	}
 
+	/* FIND */
+
 	find(selector) {
 		const element = new RQuery(this.element.querySelector(selector))
 
@@ -23,12 +27,14 @@ class RQuery {
 		}
 	}
 
-   append(childElement) {
+	/* INSERT */
+
+	append(childElement) {
 		this.element.appendChild(childElement)
 		return this
 	}
 
-   before(newElement) {
+	before(newElement) {
 		if (!(newElement instanceof HTMLElement)) {
 			throw new Error('Element must be an HTMLElement')
 		}
@@ -43,7 +49,7 @@ class RQuery {
 		}
 	}
 
-   html(htmlContent) {
+	html(htmlContent) {
 		if (typeof htmlContent === 'undefined') {
 			return this.element.innerHTML
 		} else {
@@ -52,6 +58,75 @@ class RQuery {
 		}
 	}
 
+	text(textContent) {
+		if (typeof textContent === 'undefined') {
+			return this.element.textContent
+		} else {
+			this.element.textContent = textContent
+			return this
+		}
+	}
+
+	/* EVENTS */
+
+	click(callback) {
+		this.element.addEventListener('click', callback)
+		return this
+	}
+
+	/* FORM */
+
+	input({ onInput, ...rest }) {
+		if (this.element.tagName.toLowerCase() !== 'input')
+			throw new Error('Element must be an input')
+
+		for (const [key, value] of Object.entries(rest)) {
+			this.element.setAttribute(key, value)
+		}
+
+		if (onInput) {
+			this.element.addEventListener('input', onInput)
+		}
+
+		return this
+	}
+
+	numberInput(limit) {
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'number'
+		)
+			throw new Error('Element must be an input with type "number"')
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = value
+		})
+
+		return this
+	}
+
+	creditCardInput() {
+		const limit = 16
+
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'text'
+		)
+			throw new Error('Element must be an input with type "text"')
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = formatCardNumberWithDashes(value)
+		})
+
+		return this
+	}
+
+	/* STYLES */
+
 	css(property, value) {
 		if (typeof property !== 'string' || typeof value !== 'string') {
 			throw new Error('property and value must be strings')
@@ -59,6 +134,43 @@ class RQuery {
 
 		this.element.style[property] = value
 		return this
+	}
+
+	addClass(classNames) {
+		if (Array.isArray(classNames)) {
+			for (const className of classNames) {
+				this.element.classList.add(className)
+			}
+		} else {
+			this.element.classList.add(classNames)
+		}
+
+		return this
+	}
+
+	removeClass(classNames) {
+		if (Array.isArray(classNames)) {
+			for (const className of classNames) {
+				this.element.classList.remove(className)
+			}
+		} else {
+			this.element.classList.remove(classNames)
+		}
+
+		return this
+	}
+
+	attr(attributeName, value) {
+		if (typeof attributeName !== 'string') {
+			throw new Error('Attribute name must be a string')
+		}
+
+		if (typeof value === 'undefined') {
+			return this.element.getAttribute(attributeName)
+		} else {
+			this.element.setAttribute(attributeName, value)
+			return this
+		}
 	}
 }
 
